f85fa07083d2702faed1a5c9174115707943b9d5
sushiyoung <ghldud321@naver.com>
2023-10-06 00:27:49 +0900

    변수하나로 합치기

--- workout.py
+++ workout.py
@@ -1,11 +1,13 @@
-f = open('workout.txt', 'a',encoding='utf-8')
+f = open('workout.txt', 'w',encoding='utf-8')
 
-year = input("연도를 입력하세요 :")
-month = input("월을 입력하세요 :")
-date = input("일을 입력하세요 :")
+d1 = input("날짜를 입력하세요 :")
+d2 = input("월을 입력하세요 :")
+d3 = input("일을 입려가세요 :")
+d = d1+d2+d3
 
-print("입력날짜는", year + "년", month +"월",date + "일 입니다.")
-f.write("\n날짜 :" + year + "-" + month + "-" + date)
+print(d)
+f.write("\n날짜 :  ")
+
 
 f.write("\nprepare:")
 f.write(input("prepare를 입력하세요 :"))
