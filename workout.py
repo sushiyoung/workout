9b229448ae3e058b8d7a0e14fb102f68a22828b7
sushiyoung <ghldud321@naver.com>
2023-10-12 01:06:21 +0900

    year, month, day 받는 법 by 문성이형

--- workout.py
+++ workout.py
@@ -1,33 +1,18 @@
-# with open('workout.txt', 'a', encoding='utf-8') as f:
-        
-#     year, month, day = input("날짜를 입력하세요 :").split('-')
-
-#     date = "입력날짜는 : {}년 {}월{}일 입니다.".format(year, month, day)
-
-#     print(date)
-#     f.write(date)
-
-
-#     f.write("\nprepare:")
-#     f.write(input("prepare를 입력하세요 :"))
-
-
 def input_date():
-    print(input("날짜를 입력하세요 :"))
+    year, month, day = input("날짜를 입력하세요 :").split('-')  
+    return year, month, day   
 
-
-def write_file():
+def write_file(year, month, day):    
     with open('workout.txt', 'w', encoding='utf-8') as f:
-        year, month, day = input("날짜를 입력하세요 :").split('-')
         date = "입력날짜는 : {}년 {}월{}일 입니다.".format(year, month, day)   
         f.write(date)
 
-def print_file():
+def print_file():              
     pass
 
-def main():                 # 키오스크 처럼 생각(1주문,2주문..) 무조건 메인부터 실행
-    input_date()
-    write_file()
+def main():                 
+    year, month, day = input_date()     
+    write_file(year, month, day)        
     
 if __name__ == "__main__":
     main()
