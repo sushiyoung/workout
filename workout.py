110334d514f221631c95fc16e7ab9430542c6621
sushiyoung <ghldud321@naver.com>
2023-10-13 00:03:05 +0900

    print_file 까지 완성해보았음

--- workout.py
+++ workout.py
@@ -4,15 +4,20 @@ def input_date():
 
 def write_file(year, month, day):    
     with open('workout.txt', 'w', encoding='utf-8') as f:
-        date = "입력날짜는 : {}년 {}월{}일 입니다.".format(year, month, day)   
+        date = "\n입력날짜는 : {}년 {}월{}일 입니다.".format(year, month, day)   
         f.write(date)
 
 def print_file():              
-    pass
+    with open('workout.txt', 'r', encoding='utf-8') as f:
+        workout = f.read()
+    print(workout)
+
+        
 
 def main():                 
     year, month, day = input_date()     
     write_file(year, month, day)        
-    
+    print_file()
 if __name__ == "__main__":
     main()
+
