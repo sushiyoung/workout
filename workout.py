9954b48b6fb4fcba2bef26f7e8fdeabc0527a766
sushiyoung <ghldud321@naver.com>
2023-11-17 14:57:59 +0900

    중복되는 변수 하나로 합치기

--- workout.py
+++ workout.py
@@ -3,24 +3,27 @@ import glob
 
 global file_path
 
-abspath = os.path.dirname(os.path.abspath(__file__))
-file_path = os.path.join(abspath, 'Bellgym_record')
-
 def workout_input(prompt):
     return input(f"{prompt}을 입력하세요: ")
 
-def createFolder(file_path,name):
+def get_file_path(name):
+    abspath = os.path.dirname(os.path.abspath(__file__))
+    return os.path.join(abspath, f'Bellgym_record{name}')
+    
+def createFolder(name):
     try:
-        if not os.path.exists(file_path + name):
-            os.makedirs(file_path + name)
-            print("폴더가 생성되었습니다.")
+        folder_path = get_file_path(name)
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
+            print("*"*30+f"{name} 폴더가 생성되었습니다."+"*"*30)
         else:
-            print("폴더가 이미 존재합니다.")
+            print("*"*30+f"{name} 폴더가 이미 존재합니다."+"*"*30)
     except OSError:
-        print('Error: Creating directory.' + file_path + name)
+        print('Error: Creating directory.' + folder_path)
 
 def write_file(year,month,day,prepare,mainwork,wod,bulidup,name):
     try:    
+        folder_path = get_file_path(name)
         file_name = f"{name}-{year}-{month}-{day}.txt"
         file_workout = (
             f"이름 : {name}\n"
@@ -30,27 +33,27 @@ def write_file(year,month,day,prepare,mainwork,wod,bulidup,name):
             f"오늘의 wod는 : {wod}\n"
             f"오늘의 buildup은 {bulidup}\n"
         )
-        with open(file_path+ name +'\\' + file_name, 'w', encoding='utf-8') as f:
+        with open(folder_path +'\\' + file_name, 'w', encoding='utf-8') as f:
             f.write(file_workout)
     except OSError:
         print("Error: file path" + file_name)  
 
 def read_file(year,month,day,name):              
+    folder_path = get_file_path(name)
     file_name = f"{name}-{year}-{month}-{day}.txt"
-    with open(file_path+ name +'\\' + file_name, 'r', encoding='utf-8') as f:
+    with open(folder_path +'\\' + file_name, 'r', encoding='utf-8') as f:
         file_workout = f.read()
     print(file_workout)
 
 
 def search_file():
     name = workout_input("이름")
-    folder = file_path + name
-    
-    if not os.path.exists(folder):
+    folder_path = get_file_path(name)
+    if not os.path.exists(folder_path):
         print("*"*10+f"{name}님의 폴더가 존재하지 않습니다."+"*"*10)
         return
     
-    file_name_list = glob.glob(folder+ '\\*' )
+    file_name_list = glob.glob(folder_path+ '\\*' )
     print("*"*10+ f"{name}님의 대한 리스트 파일이 존재합니다"+"*"*10)
     for f in file_name_list:
         print(f)
@@ -61,8 +64,8 @@ def search_file():
     
     print("*" * 10 + "이 파일의 대한 결과입니다" +"*" * 10+"\n")
 
-    if os.path.isfile(file_path+ name+ '\\' +file_name):
-        with open(file_path+ name +'\\' + file_name, 'r', encoding='utf-8') as f:
+    if os.path.isfile(folder_path+ '\\' +file_name):
+        with open(folder_path +'\\' + file_name, 'r', encoding='utf-8') as f:
             file_workout = f.read()
             print(file_workout)
     else:
@@ -84,7 +87,7 @@ def main():
             mainwork = workout_input("mainwork")
             wod = workout_input("WOD")
             bulidup = workout_input("Buildup")    
-            createFolder(file_path,name)
+            createFolder(name)
             write_file(year, month, day, prepare, mainwork,wod,bulidup,name)        
             read_file(year,month,day,name)
         elif choice == 2:
