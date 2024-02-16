d066f1dec8302917a2a876a6fabd6fd7e0c1f0f3
sushiyoung <ghldud321@naver.com>
2024-02-16 19:56:42 +0900

    search 객체지향 형식으로 검색기능 구현

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, render_template, flash, jsonify, Response
+from flask import Flask, request, redirect, render_template, flash, jsonify, Response, url_for
 from datetime import datetime
 import os
 import json
@@ -11,72 +11,48 @@ app = Flask(__name__)
 db = BellGymDB()
 db.connect()
 
+# ---------------------------------------------------------------------------------------------------------
 
 # localhost:5001/
 @app.route('/')
 def index():
     user_records = get_users_information()
     users = [User(record[0], record[1], record[2]) for record in user_records]
-    print("User information!!!!!!!!!:", users)
+    # print("User information!!!!!!!!!:", users)
 
     workout_records = get_workout_information()
     workouts = [Workout(record[1], record[2], record[3], record[4], record[5], record[6]) for record in workout_records]
-    print("Workout information!!!!!!!!!:", workouts)
+    # print("Workout information!!!!!!!!!:", workouts)
 
     user_workout_combine = [{'user': user, 'workout': workout}
                           for user, workout in zip(users, workouts)]
 
     
-    return render_template('index.html', user_workout_combine=user_workout_combine)
+    return render_template('index.html', user_workout_combine= user_workout_combine)
 
-
-# @app.route('/go', methods=['GET'])
-# def go():
-#     return render_template('search_result.html')
-
-# @app.route('/test', methods=['GET'])
-# def test():
-#     # user_id = request.args.get("user_id")
-#     result = get_users()
-#     data = convertToJson(result)
-#     print(data)
-#     return convertToJson(result)
 
 @app.route('/search', methods=['GET'])
 def search():
     current_year = datetime.now().year
-    user_id = request.args.get("search_id")
-    print("userid : ", user_id)
-    searched_users = search_users_information(user_id)
-    print("searched_users : " , searched_users)
-    return render_template('search_jhy.html', current_year=current_year, searched_user_list = searched_users)
-   
+    search_id = request.args.get("search_id")
+    print("Search ID: ", search_id)
+
+    if search_id:
+        searched_user_records = search_users_by_id(search_id)
+        searched_workout_records = search_workout_by_id(search_id)
+        users = [User(record[0], record[1], record[2])
+                 for record in searched_user_records]
+        workouts = [Workout(record[0], record[1], record[2], record[3], record[4], record[5])
+                    for record in searched_workout_records]
+        user_workout_combine = [{'user': user, 'workout': workout}
+                                for user, workout in zip(users, workouts)]
+        # print("User information!!!!!!!!!:", users)
+        return render_template('search_jhy.html', current_year=current_year, user_workout_combine=user_workout_combine)
+    else:
+        return ("ID를 눌러주세요!")
     
-    # result = get_user_by_id(user_id)
-    result = get_users()
-    # return jsonify(result)
-    return convertToJson(result)
-    
-    # if user:
-    #     workouts = get_workouts_for_user(user['id'])
-    #     return render_template('search.result.html', user=user, workouts=workouts)
-    # else:
-    #     flash("가입인원이 아닙니다. 회원가입을 먼저 진행해주세요!")
-    #     return redirect('/')
 
-# def convertToJson(result):
-#     return json.dumps(result, default=lambda o: o.__dict__, sort_keys=True, ensure_ascii=False)
-
-
-# def get_users():
-#     # result1 = db.select("SELECT * FROM user where id = %s", (id,))
-#     result1 = db.selectAll("SELECT * FROM user")
-
-#     if not result1:
-#         return {"ERROR": "등록된 회원이 아닙니다."}
-    
-#     user = [User(u[0], u[1], u[2]) for u in result1]
-#     return user
+# -------------------------------------------------------------------------------------------------------
 
 
 def get_users_information():
@@ -87,34 +63,26 @@ def get_workout_information():
     result1 = db.selectAll("SELECT * FROM workout")
     return result1
 
-def search_users_information(id):
+
+def search_users_by_id(id):
     result = db.selectAll(f"SELECT * FROM user where id = '{id}'")
+    # print("Result from search_users_by_id :", result)
+    return result
+
+def search_workout_by_id(id):
+    result = db.selectAll(f"SELECT * FROM workout where id = '{id}'")
+    print("Result from search_workout_by_id :", result)
+    return result
+
+def search_users_by_name(name):
+    result = db.selectAll(f"SELECT * FROM user where name = '{name}'")
+    print("Result from search_users_by_name :", name)
     return result
 
 
-# def get_workouts_for_user(id):
-#     cur = mysql.connection.cursor()
-#     cur.execute("SELECT * FROM workout where id = %s", (id,))
-#     workouts = cur.fetchall()
-#     cur.close()
-#     return workouts
-
-
-# def get_users_from_db():
-#     cur = mysql.connector.cursor()
-#     cur.execute("SELECT * FROM user")
-#     users = cur.fetchall()
-#     cur.close()
-#     print(users)
-#     return users
-
-# def get_workouts_from_db():
-#     cur = mysql.connection.cursor()
-#     cur.execute("SELECT * FROM workout")
-#     workouts = cur.fetchall()
-#     cur.close()
-#     return workouts
-
+def search_users_information(id):
+    result = db.selectAll(f"SELECT * FROM user where id = '{id}'")
+    return result
 
 
 if __name__ == '__main__':
