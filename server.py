8c84877e4989d456f39eafac9bd870cc78bfbdb5
sushiyoung <ghldud321@naver.com>
2024-02-26 00:40:34 +0900

    INNER JOIN을 사용해 코드 정리

--- server.py
+++ server.py
@@ -16,74 +16,115 @@ db.connect()
 # localhost:5001/
 @app.route('/')
 def index():
-    user_records = get_users_information()
-    users = [User(record[0], record[1], record[2]) for record in user_records]
-    # print("User information!!!!!!!!!:", users)
-
-    workout_records = get_workout_information()
-    workouts = [Workout(record[1], record[2], record[3], record[4], record[5], record[6]) for record in workout_records]
-    # print("Workout information!!!!!!!!!:", workouts)
-
-    user_workout_combine = [{'user': user, 'workout': workout}
-                          for user, workout in zip(users, workouts)]
-
+    current_year = datetime.now().year
+    user_workout_combine = get_user_workout_information()
+    # print("USER WORKOUT INFOR :" , user_workout_combine)
+    return render_template('index.html', user_workout_combine = user_workout_combine, current_year = current_year)
     
-    return render_template('index.html', user_workout_combine= user_workout_combine)
 
-
-@app.route('/search', methods=['GET'])
+@app.route('/search', methods = ['GET'])
 def search():
     current_year = datetime.now().year
-    search_id = request.args.get("search_id")
-    print("Search ID: ", search_id)
+    search_query = request.args.get("search_query")
+    print("RESULT QUERY : ", search_query)
 
-    if search_id:
-        searched_user_records = search_users_by_id(search_id)
-        searched_workout_records = search_workout_by_id(search_id)
-        users = [User(record[0], record[1], record[2])
-                 for record in searched_user_records]
-        workouts = [Workout(record[0], record[1], record[2], record[3], record[4], record[5])
-                    for record in searched_workout_records]
-        user_workout_combine = [{'user': user, 'workout': workout}
-                                for user, workout in zip(users, workouts)]
-        # print("User information!!!!!!!!!:", users)
-        return render_template('search_jhy.html', current_year=current_year, user_workout_combine=user_workout_combine)
-    else:
-        return ("ID를 눌러주세요!")
-    
+    if search_query:
+        search_results = search_user_workout(search_query)
+        user_workout_combine = search_user_workout(search_query)
+        return render_template('search_jhy.html', user_workout_combine=user_workout_combine, current_year=current_year, search_results = search_results)
+
+
 
 # -------------------------------------------------------------------------------------------------------
 
-
-def get_users_information():
-    result1 = db.selectAll("SELECT * FROM user")
-    return result1
-
-def get_workout_information():
-    result1 = db.selectAll("SELECT * FROM workout")
-    return result1
+def get_user_workout_information():
+    result = db.selectAll(
+        "SELECT user.id, user.name, workout.date, workout.prepare, workout.mainwork, workout.wod, workout.buildup FROM user INNER JOIN workout ON user.id = workout.id")
+    user_workout_combine = [{'id': record[0], 'name': record[1], 'date': record[2], 'prepare': record[3],
+                             'mainwork': record[4], 'wod': record[5], 'buildup': record[6]} for record in result]
+    return user_workout_combine
 
 
-def search_users_by_id(id):
-    result = db.selectAll(f"SELECT * FROM user where id = '{id}'")
-    # print("Result from search_users_by_id :", result)
-    return result
-
-def search_workout_by_id(id):
-    result = db.selectAll(f"SELECT * FROM workout where id = '{id}'")
-    print("Result from search_workout_by_id :", result)
-    return result
-
-def search_users_by_name(name):
-    result = db.selectAll(f"SELECT * FROM user where name = '{name}'")
-    print("Result from search_users_by_name :", name)
-    return result
-
-
-def search_users_information(id):
-    result = db.selectAll(f"SELECT * FROM user where id = '{id}'")
-    return result
-
+def search_user_workout(search_query):
+    query = (
+        "SELECT user.id, user.name, workout.date, workout.prepare, workout.mainwork, workout.wod, workout.buildup "
+        "FROM user "
+        "INNER JOIN workout ON user.id = workout.id "
+        "WHERE user.id LIKE %s"
+    )
+    # '%'를 사용하여 부분 일치하는 검색을 수행
+    search_param = '%' + search_query + '%'
+    records = db.selectAll(query, (search_param, search_param))
+    user_workout_combine = [{'id': record[0], 'name': record[1], 'date': record[2], 'prepare': record[3],
+                             'mainwork': record[4], 'wod': record[5], 'buildup': record[6]} for record in records]
+    return user_workout_combine
 
 if __name__ == '__main__':
     app.run(port=5001, debug=True)
+
+
+
+# 원본index,search
+# -------------------------------------------------------------------------------------------
+# @app.route('/')
+# def index():
+#     user_records = get_users_information()
+#     users = [User(record[0], record[1], record[2]) for record in user_records]
+#     # print("User information!!!!!!!!!:", users)
+
+#     workout_records = get_workout_information()
+#     workouts = [Workout(record[1], record[2], record[3], record[4],
+#                         record[5], record[6]) for record in workout_records]
+#     # print("Workout information!!!!!!!!!:", workouts)
+
+#     user_workout_combine = [{'user': user, 'workout': workout}
+#                             for user, workout in zip(users, workouts)]
+
+#     return render_template('index.html', user_workout_combine=user_workout_combine)
+    
+
+# @app.route('/search', methods=['GET'])
+# def search():
+#     current_year = datetime.now().year
+#     search_id = request.args.get("search_id")
+#     print("Search ID: ", search_id)
+
+#     if search_id:
+#         searched_user_records = search_users_by_id(search_id)
+#         searched_workout_records = search_workout_by_id(search_id)
+#         users = [User(record[0], record[1], record[2])
+#                  for record in searched_user_records]
+#         workouts = [Workout(record[0], record[1], record[2], record[3], record[4], record[5])
+#                     for record in searched_workout_records]
+#         user_workout_combine = [{'user': user, 'workout': workout}
+#                                 for user, workout in zip(users, workouts)]
+#         # print("User information!!!!!!!!!:", users)
+#         return render_template('search_jhy.html', current_year=current_year, user_workout_combine=user_workout_combine)
+#     else:
+#         return ("ID를 눌러주세요!")
+
+
+# def search_users_by_id(id):
+#     result = db.selectAll(f"SELECT * FROM user where id = '{id}'")
+#     # print("Result from search_users_by_id :", result)
+#     return result
+
+
+# def search_workout_by_id(id):
+#     result = db.selectAll(f"SELECT * FROM workout where id = '{id}'")
+#     print("Result from search_workout_by_id :", result)
+#     return result
+
+
+# def search_users_by_name(name):
+#     result = db.selectAll(f"SELECT * FROM user where name = '{name}'")
+#     print("Result from search_users_by_name :", name)
+#     return result
+
+
+# def search_users_information(id):
+#     result = db.selectAll(f"SELECT * FROM user where id = '{id}'")
+#     return result
+
+
+# -----------------------------------------------------------------------------------------
